03a99199445eeb269dc2d205d014d156f7ec4b44
Nguyen Hoang Nam <namnguyenjune8@gmail.com>
2023-12-30 12:45:50 +0700

    second

--- src/main.js
+++ src/main.js
@@ -3,14 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import Toaster from '@meforma/vue-toaster';
 
-
-
-
-createApp(App).use(Toaster).use(router).use(AOS).mount('#app')
-AOS.init();
+createApp(App).use(router).mount('#app')
 
      
